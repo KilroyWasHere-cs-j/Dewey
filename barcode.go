f2f99f5979eae66c1998d7f10ce66c20947f26a9
Gabriel Tower <gmtower1@proton.me>
2026-04-06 14:30:01 -0400

    Added in some basic code for dealing with barcodes(doesn't do anything yet really, just getting it in there). Adding in the libary for interfacing with the SQLite

--- barcode.go
+++ barcode.go
@@ -1,3 +1,48 @@
 package main
 
+import (
+	"image/png"
+	// "log"
+	"os"
 
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/oned"
+)
+
+func createBarCode() (*gozxing.BitMatrix, error) {
+	enc := oned.NewCode128Writer()
+
+	img, err := enc.Encode(
+		"Hello, World!",
+		gozxing.BarcodeFormat_CODE_128,
+		250,
+		50,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return img, nil
+}
+
+func createImage(filename string, img *gozxing.BitMatrix) error {
+	file, err := os.Create(filename + ".png")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return png.Encode(file, img)
+}
+//
+// func main() {
+// 	img, err := createBarCode()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	if err := createImage("barcode", img); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
