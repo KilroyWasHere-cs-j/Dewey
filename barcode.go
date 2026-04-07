a414b18576a13d4e35075cfd90bd55ff0ea67d43
Gabriel Tower <gmtower1@proton.me>
2026-04-07 14:47:39 -0400

    Adding in some the basic code for the SQL interface

--- barcode.go
+++ barcode.go
@@ -1,16 +1,22 @@
 package main
 
+/*
+	Barcode library used: github.com/makiuchi-d/gozxing
+*/
 import (
 	"errors"
+	"image"
 	"image/png"
 	"os"
+	"fmt"
 
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/oned"
+	"github.com/makiuchi-d/gozxing/qrcode"
 )
 
 /*
-	Creates a barcode
+	Creates a barcode using the Code128 format
 	# Arguments
 	- text (string)
 	- width (int)
@@ -62,3 +68,44 @@ func createImage(filename string, img *gozxing.BitMatrix) error {
 	return png.Encode(file, img)
 }
 
+func scanBarCode(path string) {
+	// open and decode image file
+	file, err := os.Open(path)
+	if err != nil {
+		Warn(err.Error())
+		return
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		Warn(err.Error())
+		return
+	}
+
+	// prepare BinaryBitmap
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		Warn(err.Error())
+		return
+	}
+
+	// decode image
+	qrReader := qrcode.NewQRCodeReader()
+	result, _ := qrReader.Decode(bmp, nil)
+
+	fmt.Println(result)
+}
+
+// Below is some GPTChat code for handling PDFs still need to test
+// doc, _ := fitz.New("file.pdf")
+// defer doc.Close()
+//
+// for n := 0; n < doc.NumPage(); n++ {
+//     img, _ := doc.Image(n)
+//
+//     bmp, _ := gozxing.NewBinaryBitmapFromImage(img)
+//     result, _ := qrReader.Decode(bmp, nil)
+//
+//     if result != nil {
+//         fmt.Println(result.String())
+//     }
+// }
--- sql.go
+++ sql.go
@@ -1,6 +1,41 @@
 package main
 
+/*
+	https://www.twilio.com/en-us/blog/developers/community/use-sqlite-go
+*/
+
+import (
+	"database/sql"
+	"log"
+	_ "github.com/mattn/go-sqlite3"
+)
+
 // DO NOT run this function unless you are Gabe
 func dbInit() {
-
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	sqlStmt := `
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name TEXT
+    );
+    `
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Table 'users' created successfully")
 }
+
+// Open db connection
+
+// Close db connection
+
+// Insert file record
+
+// Delete file record (just mark as deleted not for real)
+
+
